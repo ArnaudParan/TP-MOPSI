8b8681f320730679fe59ea1eae474078747e3197
arnaud_paran <paran.arnaud@gmail.com>
2015-11-08 09:13:02 +0100

    début tp foncteurs

--- TP1 Matrices/src/main.cpp
+++ TP1 Matrices/src/main.cpp
@@ -8,8 +8,8 @@
 int main()
 {
 	Matrice test1(3, 5), test2(5, 10);
-	for(int i = 0; i < test1.nlignes(); i++) {
-		for(int j = 0; j < test1.ncolonnes(); j++) {
+	for(unsigned int i = 0; i < test1.nlignes(); i++) {
+		for(unsigned int j = 0; j < test1.ncolonnes(); j++) {
 			test1(i, j) = rand() / (float) RAND_MAX;
 		}
 	}
